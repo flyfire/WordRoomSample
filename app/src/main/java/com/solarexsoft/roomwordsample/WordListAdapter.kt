8dff6e6ee20b4922e221d1442bb52290bc1c02ef
Solarex <rh.hou.work@gmail.com>
2021-09-06 08:34:43 +0800

    room

--- app/src/main/java/com/solarexsoft/roomwordsample/WordListAdapter.kt
+++ app/src/main/java/com/solarexsoft/roomwordsample/WordListAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -22,4 +24,26 @@ class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     fun bind(text: String?) {
         wordItemView.text = text
     }
+}
+
+class WordsComparator : DiffUtil.ItemCallback<Word>() {
+    override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+        return oldItem.word == newItem.word
+    }
+}
+
+class WordListAdapter : ListAdapter<Word, WordViewHolder>(WordsComparator()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.word)
+    }
+
 }
